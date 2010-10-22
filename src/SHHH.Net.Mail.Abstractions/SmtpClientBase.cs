f26849cd4ea717be3f28dbc77576640c6947e252
Geoff Horsey <geoff@shhhinnovations.com>
2010-10-21 21:56:03 -0700

    initial commit

---
+++ src/SHHH.Net.Mail.Abstractions/SmtpClientBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Permissions;
+
+namespace SHHH.Net.Mail.Abstractions
+{
+    public abstract class SmtpClientBase :IDisposable
+    {
+        #region Properties
+        public abstract X509CertificateCollection ClientCertificates { get; }
+        public abstract ICredentialsByHost Credentials { get; set; }
+        public abstract SmtpDeliveryMethod DeliveryMethod { get; set; }
+        public abstract bool EnableSsl { get; set; }
+        public abstract string Host { get; set; }
+        public abstract string PickupDirectoryLocation { get; set; }
+        public abstract int Port { get; set; }
+        public abstract ServicePoint ServicePoint { get; }
+        public abstract string TargetName { get; set; }
+        public abstract int Timeout { get; set; }
+        public abstract bool UseDefaultCredentials { get; set; }
+        #endregion
+
+
+        #region Methods
+        public abstract void Send(MailMessage message);
+        public abstract void Send(string from, string recipients, string subject, string body);
+
+        [HostProtectionAttribute(SecurityAction.LinkDemand, ExternalThreading = true)]
+        public abstract void SendAsync(MailMessage message, Object userToken);
+
+        [HostProtectionAttribute(SecurityAction.LinkDemand, ExternalThreading = true)]
+        public abstract void SendAsync(string from, string recipients, string subject, string body, Object userToken);
+
+        public abstract void SendAsyncCancel();
+        #endregion
+
+        #region IDisposable Members
+        public abstract void Dispose();
+        #endregion
+    }
+}
---
+++ src/SHHH.Net.Mail.Abstractions/SmtpClientWrapper.cs
@@ -0,0 +1,108 @@
+﻿using System.Net.Mail;
+
+namespace SHHH.Net.Mail.Abstractions
+{
+    public class SmtpClientWrapper : SmtpClientBase
+    {
+
+        private SmtpClient Client{ get; set;}
+        public SmtpClientWrapper(SmtpClient client)
+        {
+            Client = client;
+        }
+
+        public override System.Security.Cryptography.X509Certificates.X509CertificateCollection ClientCertificates
+        {
+            get { return Client.ClientCertificates; }
+        }
+
+        public override System.Net.ICredentialsByHost Credentials
+        {
+            get { return Client.Credentials; }
+            set { Client.Credentials = value; }
+        }
+
+        public override SmtpDeliveryMethod DeliveryMethod
+        {
+            get  { return Client.DeliveryMethod;  }
+            set { Client.DeliveryMethod = value; }
+        }
+
+        public override bool EnableSsl
+        {
+            get { return Client.EnableSsl; }
+            set { Client.EnableSsl = value; }
+        }
+
+        public override string Host
+        {
+            get { return Client.Host; }
+            set { Client.Host = value; }
+        }
+
+        public override string PickupDirectoryLocation
+        {
+            get {  return Client.PickupDirectoryLocation; }
+            set { Client.PickupDirectoryLocation = value; }
+        }
+
+        public override int Port
+        {
+            get { return Client.Port; }
+            set { Client.Port = value; }
+        }
+
+        public override System.Net.ServicePoint ServicePoint
+        {
+            get { return Client.ServicePoint; }
+        }
+
+        public override string TargetName
+        {
+            get { return Client.TargetName; }
+            set { Client.TargetName = value; }
+        }
+
+        public override int Timeout
+        {
+            get { return Client.Timeout; }
+            set { Client.Timeout = value; }
+        }
+
+        public override bool UseDefaultCredentials
+        {
+            get { return Client.UseDefaultCredentials; }
+            set { Client.UseDefaultCredentials = value; }
+        }
+
+        public override void Send(MailMessage message)
+        {
+            Client.Send(message);
+        }
+
+        public override void Send(string from, string recipients, string subject, string body)
+        {
+            Client.Send(from, recipients, subject, body);
+        }
+
+        public override void SendAsync(MailMessage message, object userToken)
+        {
+            Client.SendAsync(message, userToken);
+        }
+
+        public override void SendAsync(string from, string recipients, string subject, string body, object userToken)
+        {
+            Client.SendAsync(from, recipients, subject, body, userToken);
+        }
+
+        public override void SendAsyncCancel()
+        {
+            Client.SendAsyncCancel();
+        }
+
+        public override void Dispose()
+        {
+            Client.Dispose();
+        }
+    }
+}
